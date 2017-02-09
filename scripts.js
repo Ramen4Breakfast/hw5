c74688ea855857613509b13f0a0b4e0f43b5a933
Gavin Roy <Gavin@Gavins-MacBook-Pro.local>
2017-02-08 18:53:53 -0800

    Added a nearly completed website

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function addOlives() {
+  document.getElementById('para1').innerHTML = "Olives ";
+}
+
+function addPeppers() {
+  document.getElementById('para2').innerHTML = "Peppers ";
+}
+
+function addMushrooms() {
+  document.getElementById('para3').innerHTML = "Mushrooms ";
+}
+
+function clearAll()
+
+{
+  document.getElementById('para1').innerHTML = "";
+  document.getElementById('para2').innerHTML = "";
+  document.getElementById('para3').innerHTML = "";
+}
