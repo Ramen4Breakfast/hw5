a32a3adf9862d231668024c708e0eee96fdd3bea
Gavin Roy <Gavin@Gavins-MacBook-Pro.local>
2017-02-08 21:14:42 -0800

    fixed final bug

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-function addOlives() {
-  document.getElementById('para1').innerHTML = "Olives ";
-}
-
-function addPeppers() {
-  document.getElementById('para2').innerHTML = "Peppers ";
-}
-
-function addMushrooms() {
-  document.getElementById('para3').innerHTML = "Mushrooms ";
-}
-
 function clearAll()
 
 {
