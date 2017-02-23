4c1b4f60f9e6fe1f56f4688dde39cb4baeb27165
Gavin Roy <Gavin@Gavins-MacBook-Pro.local>
2017-02-22 21:57:54 -0800

    cleaned up code

--- scripts.js
+++ scripts.js
@@ -2,64 +2,46 @@ var total = 0;
 var receipt = [];
 arrayLength = 0
 
-function addMushroom()
-{
-	total++;
-	document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
-	receipt.push ("Mushrooms");
+function addMushroom() {
+    total++;
+    document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
+    receipt.push("Mushrooms");
 }
 
-function addOlive()
-{
-	total++;
-	document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
-	receipt.push ("Olives");
+function addOlive() {
+    total++;
+    document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
+    receipt.push("Olives");
 }
 
-function addPepper()
-{
-	total++;
-	document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
-	receipt.push ("Peppers");
+function addPepper() {
+    total++;
+    document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
+    receipt.push("Peppers");
 }
 
-function clearAll()
-
-{
-	
-	total = 0;
-	receipt = []
-  	document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
-  	document.getElementById('receipts').innerHTML = "Please Select Toppings";
-
+function clearAll() {
+    total = 0;
+    receipt = []
+    document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
+    document.getElementById('receipts').innerHTML = "Please Select Toppings";
 }
 
-function typeTotal()
-
-{
-	document.getElementById('typeTotal').innerHTML = "Your Number of Toppings: " + total;
-
+function typeTotal() {
+    document.getElementById('typeTotal').innerHTML = "Your Number of Toppings: " + total;
 }
 
-function printReceipt()
-{
-
-	document.getElementById("receipts").innerHTML = receipt;
-
+function printReceipt() {
+    document.getElementById("receipts").innerHTML = receipt;
 }
 
 function Ordering() {
-  
-  	var myString = document.getElementById("order").value;
-  	console.log(myString);
-	var Order = myString.split(",");
-	for (var i = 0; i < Order.length; i++) {
-		receipt.push(Order[i]);
-	}
-
-	total = receipt.length;
-	document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
-
-
-
-	}
+    var myString = document.getElementById("order").value;
+    console.log(myString);
+    var Order = myString.split(",");
+    for (var i = 0; i < Order.length; i++) {
+        receipt.push(Order[i]);
+    }
+    total = receipt.length;
+    document.getElementById('total').innerHTML = "Your Number of Toppings: " + total;
+}
